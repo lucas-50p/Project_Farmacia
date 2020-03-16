52d159771f3db6c5b8f6549e648027a8d9da620f
lucas bueno <lucas-50p>
2020-03-16 10:52:38 -0300

    Capturar Class

--- src/main/java/br/com/lucas/drogaria/dao/GenericDAO.java
+++ src/main/java/br/com/lucas/drogaria/dao/GenericDAO.java
@@ -1,11 +1,22 @@
 package br.com.lucas.drogaria.dao;
 
+import java.lang.reflect.ParameterizedType;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import br.com.lucas.drogaria.util.HibernateUtil;
 
+
 public class GenericDAO <Entidade>{
+	
+	private Class<Entidade> classe;
+
+	@SuppressWarnings("unchecked")
+	public GenericDAO() {
+		this.classe = (Class<Entidade>) ((ParameterizedType) getClass().getGenericSuperclass())
+				.getActualTypeArguments()[0];
+	}
 	public void salvar(Entidade entidade) {
 		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
 		Transaction transacao = null;

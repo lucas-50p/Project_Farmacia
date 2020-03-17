c8f1795ac55a5ffeb6deaf4ad94feb409d5ab3c9
lucas bueno <lucas-50p>
2020-03-17 11:21:17 -0300

    Listar e Buscar

--- src/test/java/br/com/lucas/drogaria/dao/CidadeDAOTest.java
+++ src/test/java/br/com/lucas/drogaria/dao/CidadeDAOTest.java
@@ -1,5 +1,8 @@
 package br.com.lucas.drogaria.dao;
 
+import java.util.List;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
 import br.com.lucas.drogaria.domain.Cidade;
@@ -8,16 +11,51 @@ import br.com.lucas.drogaria.domain.Estado;
 public class CidadeDAOTest {
 
 	@Test
+	@Ignore
 	public void salvar() {
 		EstadoDAO estadoDAO = new EstadoDAO();
-		
+
 		Estado estado = estadoDAO.buscar(2L);
-		
+
 		Cidade cidade = new Cidade();
 		cidade.setNome("Santa Cruz");
 		cidade.setEstado(estado);
-	
+
 		CidadeDAO cidadeDAO = new CidadeDAO();
 		cidadeDAO.salvar(cidade);
+	}
+	@Test
+	@Ignore
+	public void listar() {
+		CidadeDAO cidadeDAO = new CidadeDAO();
+		List<Cidade> resultado = cidadeDAO.listar();
+		
+		for(Cidade cidade : resultado) {
+			System.out.println("Código da Cidade: " + cidade.getCodigo());
+			System.out.println("Nome da Cidade: " + cidade.getNome());
+			System.out.println("Código do Estado: " + cidade.getEstado().getCodigo());
+			System.out.println("Sigla do Estado: " + cidade.getEstado().getSigla());
+			System.out.println("Nome do Estado: " + cidade.getEstado().getNome());
+			System.out.println();
+		}
+	}
+	
+	@Test
+	public void buscar() {
+		Long codigo = 3L;
+		
+		CidadeDAO cidadeDAO = new CidadeDAO();
+		Cidade cidade = cidadeDAO.buscar(codigo);
+		
+	
+			System.out.println("Código da Cidade: " + cidade.getCodigo());
+			System.out.println("Nome da Cidade: " + cidade.getNome());
+			System.out.println("Código do Estado: " + cidade.getEstado().getCodigo());
+			System.out.println("Sigla do Estado: " + cidade.getEstado().getSigla());
+			System.out.println("Nome do Estado: " + cidade.getEstado().getNome());
+			System.out.println();
+			
+			
+		
 	}
 }

d2f65b43af5a8eab31827f2eedaf6c65e6016e8f
lucas bueno <lucas-50p>
2020-03-19 14:21:36 -0300

    ManagedBean, Estado Bean

---
+++ src/main/java/br/com/lucas/drogaria/domain/bean/EstadoBean.java
@@ -0,0 +1,10 @@
+package br.com.lucas.drogaria.domain.bean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean//tratar do controle e do modelo dentro da nossa aplicação
+public class EstadoBean {
+	public void salvar(){
+		System.out.println("Programação Web com Java");
+	}
+}

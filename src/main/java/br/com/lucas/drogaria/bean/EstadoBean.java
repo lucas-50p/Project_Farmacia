bd5e79c955293636c9754d553e3901ab4a07131d
lucas bueno <lucas-50p>
2020-03-25 13:03:44 -0300

    EstadoBean - Ordenação, Filtragem e Paginação

--- src/main/java/br/com/lucas/drogaria/bean/EstadoBean.java
+++ src/main/java/br/com/lucas/drogaria/bean/EstadoBean.java
@@ -19,9 +19,9 @@ import br.com.lucas.drogaria.domain.Estado;
  * Severity: é o tipo da mensagem, impactar no tipo da cor do primeFace exibi para nos
  * SEVERITY_INFO:Informação, na tela
  * SEVERITY_ERROR: na tela erro*/
+
 @SuppressWarnings("serial")
-@ManagedBean // tratar do controle e do modelo dentro da nossa aplicação,Dados que conversam
-				// com a tela
+@ManagedBean // tratar do controle e do modelo dentro da nossa aplicação,Dados que conversam com a tela
 @ViewScoped // Tempo de tela, ficam vivos enquanto estou na tela de estado
 public class EstadoBean implements Serializable {
 	private Estado estado;

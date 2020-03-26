18a78104840d0427b622160573e095ae92e1c268
lucas bueno <lucas-50p>
2020-03-26 19:09:27 -0300

    CidadeBean - Listar

---
+++ src/main/java/br/com/lucas/drogaria/bean/CidadeBean.java
@@ -0,0 +1,40 @@
+package br.com.lucas.drogaria.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.omnifaces.util.Messages;
+
+import br.com.lucas.drogaria.dao.CidadeDAO;
+import br.com.lucas.drogaria.domain.Cidade;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@ViewScoped
+public class CidadeBean implements Serializable {
+	private List<Cidade> cidades;
+	
+	public List<Cidade> getCidades() {
+		return cidades;
+	}
+
+	public void setCidades(List<Cidade> cidades) {
+		this.cidades = cidades;
+	}
+	
+	@PostConstruct//Chamar quando a tela criada
+	public void listar() {
+		try {
+			CidadeDAO cidadeDAO = new CidadeDAO();
+			cidades = cidadeDAO.listar();
+			
+		} catch (RuntimeException erro) {
+			Messages.addFlashGlobalError("Ocorreu um erro ao tentar listar as cidades");
+			erro.printStackTrace();
+		}
+	}
+}

40e2e438cdcf9daf9a2d7e13c17d97ec634668f3
lucas bueno <lucas-50p>
2020-04-02 17:34:50 -0300

    FabricanteService - Listar

---
+++ src/main/java/br/com/lucas/drogaria/service/FabricanteService.java
@@ -0,0 +1,28 @@
+package br.com.lucas.drogaria.service;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import com.google.gson.Gson;
+
+import br.com.lucas.drogaria.dao.FabricanteDAO;
+import br.com.lucas.drogaria.domain.Fabricante;
+
+//Json é uma forma textual de representar as coisas, ele gasta menos espaços
+//http://127.0.0.1:8080/Drogaria/rest/fabricante
+@Path("fabricante")
+public class FabricanteService {
+	@GET //Retornar
+	public String listar() {
+		FabricanteDAO fabricanteDAO = new FabricanteDAO();
+		List<Fabricante> fabricantes = fabricanteDAO.listar("descricao");
+		
+		Gson gson = new Gson();
+		String json = gson.toJson(fabricantes);
+		
+		return json;
+	}
+	
+}

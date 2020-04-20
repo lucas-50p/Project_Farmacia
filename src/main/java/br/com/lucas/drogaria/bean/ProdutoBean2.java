4136533c6d808cbd316a6db5f117c84a4aae1320
lucas bueno <lucas-50p>
2020-04-20 17:23:49 -0300

    Busca de Produtos - Parte 3

--- src/main/java/br/com/lucas/drogaria/bean/ProdutoBean2.java
+++ src/main/java/br/com/lucas/drogaria/bean/ProdutoBean2.java
@@ -6,6 +6,9 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.view.ViewScoped;
 
+import org.omnifaces.util.Messages;
+
+import br.com.lucas.drogaria.dao.ProdutoDAO;
 import br.com.lucas.drogaria.domain.Produto;
 
 @SuppressWarnings("serial")
@@ -13,6 +16,7 @@ import br.com.lucas.drogaria.domain.Produto;
 @ViewScoped
 public class ProdutoBean2 implements Serializable{
 	private Produto produto;
+	private Boolean exibiPainelDados;
 
 	public Produto getProduto() {
 		return produto;
@@ -22,8 +26,40 @@ public class ProdutoBean2 implements Serializable{
 		this.produto = produto;
 	}
 	
+	public Boolean getExibiPainelDados() {
+		return exibiPainelDados;
+	}
+
+	public void setExibiPainelDados(Boolean exibiPainelDados) {
+		this.exibiPainelDados = exibiPainelDados;
+	}
+	
 	@PostConstruct//Quando a tela for carregada
 	public void novo() {
-		produto = new Produto();
+	
+			produto = new Produto();
+			exibiPainelDados = false;
+	
+		
 	}
+	
+	public void buscar() {
+		try {
+			ProdutoDAO produtoDAO = new ProdutoDAO();
+			Produto resultado = produtoDAO.buscar(produto.getCodigo());
+			
+			if (resultado == null) {
+				exibiPainelDados = false;
+				Messages.addGlobalWarn("Não existe produto cadastrado para o código");
+			} else {
+				exibiPainelDados = true;
+				produto = resultado;
+			}
+		
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar buscar o produto");
+			erro.printStackTrace();
+		}
+	}
+
 }

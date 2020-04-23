59358b9bb6889bccecec36f730b833be1b652128
lucas bueno <lucas-50p>
2020-04-23 16:17:11 -0300

    Schedule - Tela

---
+++ src/main/java/br/com/lucas/drogaria/bean/CaixaBean.java
@@ -0,0 +1,28 @@
+package br.com.lucas.drogaria.bean;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.primefaces.model.DefaultScheduleModel;
+import org.primefaces.model.ScheduleModel;
+
+@ManagedBean
+@ViewScoped
+public class CaixaBean {
+	
+	private ScheduleModel caixas;
+
+	public ScheduleModel getCaixas() {
+		return caixas;
+	}
+
+	public void setCaixas(ScheduleModel caixas) {
+		this.caixas = caixas;
+	}
+	
+	@PostConstruct
+	public void listar() {
+		caixas = new DefaultScheduleModel();
+	}
+}
